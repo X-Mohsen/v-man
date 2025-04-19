4eec74db354d0df80891ccd146e33f1bb8ab6952
X-Mohsen <MaybeEvmo@gmail.com>
2025-04-19 23:26:04 +0330

    feat: add daemon mode and disk check to v-run.sh
    - Added optional --daemon/-d flag to run VMs in background
    - Added check for disk file existence before running
    - Improved list output to show clean .qcow2 names
    - Commands are composed into a single QEMU_CMD for flexibility

--- v-run.sh
+++ v-run.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
-arg=$1
+arg="$1"
+arg2="$2"
 
+# If user wants to list available VM disk names
 if [ "$arg" == "list" ]; then
-  disks=$(ls disks)
-
-  for disk in $disks; do
-    disk_name_no_suffix=$(basename "$disk" .qcow2)
-    echo "$disk_name_no_suffix"
+  for disk in disks/*.qcow2; do
+    basename "$disk" .qcow2
   done
   exit 0
 fi
 
+disk_path="./disks/$arg.qcow2"
 
-sudo qemu-system-x86_64 \
+# Check if the disk file exists
+if [ ! -f "$disk_path" ]; then
+  echo "Disk '$disk_path' does not exist!"
+  exit 1
+fi
+
+# Base QEMU command
+QEMU_CMD="sudo qemu-system-x86_64 \
   -enable-kvm \
   -m 4096 \
   -smp 2 \
   -cpu host \
-  -drive file="./disks/$arg.qcow2",format=qcow2 \
+  -drive file=\"$disk_path\",format=qcow2 \
   -boot order=c \
   -net nic \
-  -net bridge,br=br0
+  -net bridge,br=br0"
+
+# Add daemonize flags if requested
+if [ "$arg2" == "--daemon" ] || [ "$arg2" == "-d" ]; then
+  mkdir -p "daemons"
+  QEMU_CMD="$QEMU_CMD -pidfile ./daemons/$arg.pid -daemonize -display none"
+fi
+
+# Execute the command
+eval "$QEMU_CMD"
